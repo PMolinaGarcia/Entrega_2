f810c3380cbb6fd17fc96d07f7bce10d043bc80e
PABLO MOLINA GARCÍA <pablo.molinagarcia1@usp.ceu.es>
2025-02-21 13:14:52 +0100

    Subida ejercicios

---
+++ src/Metodos.java
@@ -0,0 +1,210 @@
+import java.util.*;
+import java.util.function.DoubleUnaryOperator;
+import java.util.function.Function;
+
+public class Metodos {
+
+    //Ejercicio 1: explique qué es un método recursivo.
+    //Es un tipo de método que resuelve problemas de una forma similar a la inducción matemática,
+    // conociendo la solución correcta para un caso base y llamándose a sí mismo para resolver el problema
+    // hasta llegar al caso base.
+
+    //Ejercicio 2: programe las siguientes funciones mediante métodos recursivos:
+
+    public static int sumaNTerminos (int numero){
+        if (numero==0){
+            return 0;
+        }
+        return sumaNTerminos(numero-1)+numero;
+    }
+
+    public static int sumaNTerminosMemoria (int n, int i, int suma){
+        if (i>n){
+            return suma;
+        }
+        else {
+            return sumaNTerminosMemoria(n, i+1, suma+i);
+        }
+    }
+
+    public static int factorial (int numero){
+        if (numero==0){
+            return 1;
+        }
+        return factorial(numero-1)*numero;
+    }
+
+    public static int factorialMemoria (int numero, int i, int factorial){
+        while (i==0){
+            i+=1;
+        }
+        if (i>numero){
+            return factorial;
+        }
+        else{
+            return factorialMemoria(numero, i+1, factorial*i);
+        }
+    }
+
+    public static int potencia (int base, int exponente){
+        if (exponente==0){
+            return 1;
+        }
+        return potencia(base, exponente-1)*base;
+    }
+
+    public static int sumaListaNumeros (List<Integer> listaDada, int numero) {
+        if (numero==0) {
+            return 0;
+        }
+        return sumaListaNumeros(listaDada, numero - 1) + listaDada.get(numero - 1);
+    }
+
+    public static int sumaListaNumerosMemoria (List<Integer> listaDada, int n, int i, int suma){
+        if (i==n){
+            return suma;
+        }
+        else{
+            return sumaListaNumerosMemoria(listaDada, n, i+1, suma+listaDada.get(i));
+        }
+    }
+
+    public static double mediaListaNumerosMemoria (List<Integer> listaDada, int n, int i, int suma){
+        if (i==n){
+            return (double) suma/n;
+        }
+        else{
+            return mediaListaNumerosMemoria(listaDada, n, i+1, suma+listaDada.get(i));
+        }
+    }
+
+    public static double desvTipListaMemoria (List<Integer> listaDada, int n, int i, double sumatorio){
+        if (i==n-1){
+            return Math.sqrt(sumatorio/n);
+        }
+        return desvTipListaMemoria(listaDada, n, i+1, sumatorio+Math.pow(listaDada.get(i)-mediaListaNumerosMemoria(listaDada, listaDada.size(), 0, 0),2));
+    }
+
+    public static int sumaParesHastaN (int n){
+        if (n<2){
+            return 0;
+        }
+        if (n==2){
+            return n;
+        }
+        else {
+            while (n%2==1)
+                n-=1;
+            return sumaParesHastaN(n-1)+n;
+        }
+    }
+
+    public static int sumaParesHastaNMemoria (int n, int i, int suma){
+        if (i>n){
+            return suma;
+        }
+        else {
+            while(i%2==1){
+                i-=1;
+            }
+            return sumaParesHastaNMemoria(n, i+2, suma+i);
+        }
+    }
+
+    public static int sumaParesListaMemoria (List<Integer> listaDada, int n, int i, int suma){
+        if (i==n){
+            return suma;
+        }
+        else {
+            if (listaDada.get(i)%2==1){
+                return sumaParesListaMemoria(listaDada, n, i+1, suma);
+            }
+            return sumaParesListaMemoria(listaDada, n, i+1, suma+listaDada.get(i));
+        }
+    }
+
+    public static List<Integer> listaDeParesDeLista (List<Integer> listaDada, int indice){
+        if (indice==0){
+            return new ArrayList<>();
+        }
+        else {
+            List<Integer> listaResultado = listaDeParesDeLista(listaDada, indice-1);
+            if (listaDada.get(indice-1)%2==0){
+                listaResultado.add(listaDada.get(indice-1));
+            }
+            return listaResultado;
+        }
+    }
+
+    public static List<Integer> listaDeParesHastaN (int n){
+        if (n<=2){
+            return new ArrayList<>();
+        }
+        else{
+            List<Integer> listaResultado = listaDeParesHastaN(n-1);
+            if (n%2==0){
+                listaResultado.add(n);
+            }
+            return listaResultado;
+        }
+    }
+
+    public static int productoEscalar (List<Integer> vector1, List<Integer> vector2, int n){
+        if (n==0){
+            return 0;
+        }
+        else {
+            return productoEscalar(vector1, vector2, n-1) + vector1.get(n-1)*vector2.get(n-1);
+        }
+    }
+
+    public static int terminoNFibonacci (int n){
+        if (n<=0){
+            return 0;
+        }
+        if (n==1){
+            return 1;
+        }
+        else {
+            return terminoNFibonacci(n-1) + terminoNFibonacci(n-2);
+        }
+    }
+
+    public static void torresDeHanoi (int discos, char torreOrigen, char torreAuxiliar, char torreDestino) {
+        if (discos==1){
+            System.out.println("Movemos el disco de " + torreOrigen + " a " + torreDestino);
+        }
+        else {
+            torresDeHanoi(discos-1, torreOrigen, torreDestino, torreAuxiliar);
+            System.out.println("Movemos el disco de " + torreOrigen + " a " + torreDestino);
+            torresDeHanoi(discos-1, torreAuxiliar, torreOrigen, torreDestino);
+        }
+    }
+
+    public static double integral (DoubleUnaryOperator funcion, double limInf, double limSup, int n){
+        double h = (limSup-limInf)/n;
+        return integralEXCuadrado(funcion, limInf,  limSup,  h);
+    }
+
+    public static double integralEXCuadrado (DoubleUnaryOperator funcion, double limInf, double limSup, double h){
+        if (Math.abs(limSup-limInf)<0.0001) {
+            return 0;
+        }
+        else {
+            return integralEXCuadrado(funcion, limInf+h,limSup,h)+funcion.applyAsDouble(limInf)*h;
+        }
+    }
+
+    public static int sumaCodigos(String str, int indice, int longitudCadena) {
+        if (indice==longitudCadena){
+            return str.charAt(indice);
+        }
+        else {
+            return sumaCodigos(str, indice,longitudCadena)+str.charAt(indice);
+        }
+    }
+
+
+
+
+}
---
+++ src/Principal.java
@@ -0,0 +1,80 @@
+import java.util.*;
+
+public class Principal {
+
+    public static void main (String[] args){
+
+        int numeroNTerminos = 10;
+        System.out.println("\n\nLa suma de los " + numeroNTerminos + " primeros numeros es " + Metodos.sumaNTerminos(numeroNTerminos));
+        System.out.println("\n\nLa suma de los " + numeroNTerminos + " primeros numeros es " + Metodos.sumaNTerminosMemoria(numeroNTerminos,0,0));
+
+        int numeroFactorial = 10;
+        System.out.println("\n\nEl factorial de " + numeroFactorial + " es " + Metodos.factorial(numeroFactorial));
+        System.out.println("\n\nEl factorial de " + numeroFactorial + " es " + Metodos.factorialMemoria(numeroFactorial,0,1));
+
+        int basePotencia = 5;
+        int exponentePotencia = 10;
+        System.out.println("\n\n" + basePotencia + " elevado a " + exponentePotencia + " es " + Metodos.potencia(basePotencia, exponentePotencia));
+
+        Integer[] arrayDadoSuma = {2,3,1,4,8};
+        List<Integer> listaDadaSuma = Arrays.asList(arrayDadoSuma);
+        System.out.println("\n\nLa suma de los elementos de la lista " + listaDadaSuma + " es " + Metodos.sumaListaNumeros(listaDadaSuma, listaDadaSuma.size()));
+        System.out.println("\n\nLa suma de los elementos de la lista " + listaDadaSuma + " es " + Metodos.sumaListaNumerosMemoria(listaDadaSuma, listaDadaSuma.size(), 0, 0));
+
+        Integer[] arrayDadoMedia = {1,7,45,32,23,1};
+        List<Integer> listaDadaMedia = Arrays.asList(arrayDadoMedia);
+        System.out.println("\n\nLa media de los elementos de la lista " + listaDadaMedia + " es " + Metodos.mediaListaNumerosMemoria(listaDadaMedia, listaDadaMedia.size(), 0, 0));
+
+        Integer[] arrayDadoDesvTip = {1,4,2,87,4,23};
+        List<Integer> listaDadaDesvTip = Arrays.asList(arrayDadoDesvTip);
+        System.out.println("\n\nLa desviacion tipica de los elementos de la lista " + listaDadaDesvTip + " es " + Metodos.desvTipListaMemoria(listaDadaDesvTip, listaDadaDesvTip.size(), 0, 0));
+
+        int numeroNSumaPares = 13;
+        System.out.println("\n\nLa suma de los pares hasta " + numeroNSumaPares + " es " + Metodos.sumaParesHastaN(numeroNSumaPares));
+        System.out.println("\n\nLa suma de los pares hasta " + numeroNSumaPares + " es " + Metodos.sumaParesHastaNMemoria(numeroNSumaPares, 0, 0));
+
+        Integer[] arrayDadoSumaPares = {1,1,26,3,5,34,23};
+        List<Integer> listaDadaSumaPares = Arrays.asList(arrayDadoSumaPares);
+        System.out.println("\n\nLa suma de los pares de la lista " + listaDadaSumaPares + " es " + Metodos.sumaParesListaMemoria(listaDadaSumaPares, listaDadaSumaPares.size(),0, 0));
+
+        Integer[] arrayDadoListaPares = {1,2,3,4,5,6,7,13,38};
+        List<Integer> listaDadaListaPares = Arrays.asList(arrayDadoListaPares);
+        System.out.println("\n\nLa lista de los pares de la lista " + listaDadaListaPares + " es " + Metodos.listaDeParesDeLista(listaDadaListaPares, listaDadaListaPares.size()));
+
+        int numeroHastaListaPares = 32;
+        System.out.println("\n\nLa lista de los pares hasta el numero " + numeroHastaListaPares + " es " + Metodos.listaDeParesHastaN(numeroHastaListaPares));
+
+        Integer[] arrayVector1 = {1,2,3,4};
+        Integer[] arrayVector2 = {2,3,4,5};
+        List<Integer> vector1 = Arrays.asList(arrayVector1);
+        List<Integer> vector2 = Arrays.asList(arrayVector2);
+        System.out.println("\n\nEl producto vectorial de " + vector1 + " y " + vector2 + " es " + Metodos.productoEscalar(vector1, vector2, vector1.size()));
+
+        int nFibonacci = 10;
+        System.out.println("\n\nEl termino " + nFibonacci + " de la sucesion de Fibonacci es " + Metodos.terminoNFibonacci(nFibonacci-1));
+
+        int discosHanoi = 3;
+        char torreOrigen = 'A';
+        char torreAuxiliar = 'B';
+        char torreDestino = 'C';
+        System.out.println("\n\nVamos a solucionar el juego de las torres de Hanoi con " + discosHanoi + " discos y las torres de origen " + torreOrigen + ", auxiliar " + torreAuxiliar + " y de destino " + torreDestino + ": \n");
+        Metodos.torresDeHanoi(discosHanoi, torreOrigen, torreAuxiliar, torreDestino);
+
+        double limInf = 0;
+        double limSup = 1;
+        int pasoIntegral = 20;
+        System.out.println("\n\nLa integral definida entre " + limInf + " y " + limSup + ", con un paso de " + pasoIntegral + " de e elevado a x al cuadrado es " + Metodos.integral(x->Math.exp(Math.pow(x,2)),limInf, limSup, pasoIntegral));
+
+        String cadena = "String";
+        String S = "S";
+        String t = "t";
+        String r = "r";
+        String i = "i";
+        String n = "n";
+        String g = "g";
+        int longitudCadena = cadena.length();
+        System.out.println("\n\nLa suma de los códigos de los caracteres de la palabra " + cadena + " es " + Metodos.sumaCodigos(cadena, 0, longitudCadena));
+        System.out.println("\n\nLa suma de los códigos de los caracteres de la palabra " + S + " es " + Metodos.sumaCodigos(S, 0, longitudCadena));
+    }
+
+}
